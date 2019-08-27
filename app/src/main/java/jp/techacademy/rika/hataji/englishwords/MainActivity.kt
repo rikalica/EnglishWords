6561ad85c38a6aabe33482dcdf8f155f8979b22f
rikalica <hatajimui@yahoo.co.jp>
2019-08-27 15:12:46 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/rika/hataji/englishwords/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.techacademy.rika.hataji.englishwords
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
