85783ac78f6abff35a772e6f5eaa69b6f8aeea2c
rikalica <hatajimui@yahoo.co.jp>
2019-08-28 12:37:49 +0900

    ログイン画面

--- app/src/main/java/jp/techacademy/rika/hataji/englishwords/MainActivity.kt
+++ app/src/main/java/jp/techacademy/rika/hataji/englishwords/MainActivity.kt
@@ -2,11 +2,29 @@ package jp.techacademy.rika.hataji.englishwords
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener { _ ->
+            // ログイン済みのユーザーを取得する
+            val user = FirebaseAuth.getInstance().currentUser
+
+            // ログインしていなければログイン画面に遷移させる
+            if (user == null) {
+                val intent = Intent(applicationContext, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
