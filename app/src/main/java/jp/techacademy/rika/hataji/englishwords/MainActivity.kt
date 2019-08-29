ea66f25da07c9179d47ad65683cc05f66cc8e641
rikalica <hatajimui@yahoo.co.jp>
2019-08-29 16:03:48 +0900

    メイン画面

--- app/src/main/java/jp/techacademy/rika/hataji/englishwords/MainActivity.kt
+++ app/src/main/java/jp/techacademy/rika/hataji/englishwords/MainActivity.kt
@@ -2,18 +2,23 @@ package jp.techacademy.rika.hataji.englishwords
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
 import android.view.Menu
 import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import android.content.Intent
+import android.support.v7.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mToolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mToolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(mToolbar)
+
 
         val fab = findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener { _ ->
