d4665e83a8516c4d97c9b1b151082eb98e26a459
rikalica <hatajimui@yahoo.co.jp>
2019-09-10 16:05:07 +0900

    単語暗記ページ

--- app/src/main/java/jp/techacademy/rika/hataji/englishwords/MainActivity.kt
+++ app/src/main/java/jp/techacademy/rika/hataji/englishwords/MainActivity.kt
@@ -3,29 +3,19 @@ package jp.techacademy.rika.hataji.englishwords
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-//import android.support.v7.widget.Toolbar
-import android.util.Base64
-import android.util.Log
 import android.widget.ListView
 import android.widget.Toolbar
 import com.google.firebase.database.*
-import java.util.*
 import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var mToolbar: Toolbar
     private lateinit var mDatabaseReference: DatabaseReference
     private lateinit var mListView: ListView
     private lateinit var mTrackArrayList: ArrayList<Track>
     private lateinit var mWordArrayList: ArrayList<Word>
     private lateinit var mTrackListAdapter: TrackListAdapter
-    private var mGenre = 0
 
     private var mGenreRef: DatabaseReference? = null
-    private var mTimer: Timer? = null
-    private var mHandler = Handler()
 
     private val mTrackListener = object : ChildEventListener {
         override fun onChildAdded(dataSnapshot: DataSnapshot, s: String?) {
@@ -54,14 +44,14 @@ class MainActivity : AppCompatActivity() {
 
             for(wordUnity in map) {
                 if(wordUnity != null) {
-                    val num = wordUnity["num"] ?: ""
-                    val word = wordUnity["word"] ?: ""
-                    val wordClass = wordUnity["wordClass"] ?: ""
-                    val wordMean = wordUnity["wordMean"] ?: ""
-                    val sentence = wordUnity["sentence"] ?: ""
-                    val sentenceMean = wordUnity["sentenceMean"] ?: ""
+                    var num = wordUnity["num"] ?: ""
+                    var word = wordUnity["word"] ?: ""
+                    var wordClass = wordUnity["wordClass"] ?: ""
+                    var wordMean = wordUnity["wordMean"] ?: ""
+                    var sentence = wordUnity["sentence"] ?: ""
+                    var sentenceMean = wordUnity["sentenceMean"] ?: ""
 
-                    val wordList = Word(num, word, wordClass, wordMean, sentence, sentenceMean)
+                    var wordList = Word(num, word, wordClass, wordMean, sentence, sentenceMean)
                     mWordArrayList.add(wordList)
                 }
             }
@@ -81,53 +71,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private val mEventListener = object : ChildEventListener {
-        override fun onChildAdded(dataSnapshot: DataSnapshot, s: String?) {
-            val map = dataSnapshot.value as Map<String, String>
-            val num = map["num"] ?: ""
-            val word = map["word"] ?: ""
-            val wordClass = map["wordClass"] ?: ""
-            val wordMean = map["wordMean"] ?: ""
-            val wordSentence = map["wordSentence"] ?: ""
-            val wordSentenceMean = map["wordSentenceMean"] ?: ""
-
-//            val answerArrayList = ArrayList<Answer>()
-//            val answerMap = map["answers"] as Map<String, String>?
-//            if (answerMap != null) {
-//                for (key in answerMap.keys) {
-//                    val temp = answerMap[key] as Map<String, String>
-//                    val answerBody = temp["body"] ?: ""
-//                    val answerName = temp["name"] ?: ""
-//                    val answerUid = temp["uid"] ?: ""
-//                    val answer = Answer(answerBody, answerName, answerUid, key)
-//                    answerArrayList.add(answer)
-//                }
-//            }
-
-            val words = Word(num, word, wordClass, wordMean, wordSentence, wordSentenceMean)
-            mWordArrayList.add(words)
-            mTrackListAdapter.notifyDataSetChanged()
-        }
-
-        override fun onChildChanged(dataSnapshot: DataSnapshot, s: String?) {
-        }
-
-        override fun onChildRemoved(p0: DataSnapshot) {
-
-        }
-
-        override fun onChildMoved(p0: DataSnapshot, p1: String?) {
-
-        }
-
-        override fun onCancelled(p0: DatabaseError) {
-
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mWordArrayList = ArrayList<Word>()
 
         // ListViewの準備
         mListView = findViewById(R.id.listView)
